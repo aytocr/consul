f9a933d34dfb2ea525cbe144ab551cfc165b85b8
CIM Internet <jmoreno@cim.es>
2019-04-24 13:26:41 +0200

    Menores

--- config/application.rb
+++ config/application.rb
@@ -18,29 +18,11 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :es
     available_locales = [
-      "ar",
-      "de",
       "en",
       "es",
-      "fa",
-      "fr",
-      "gl",
-      "he",
-      "id",
-      "it",
-      "nl",
-      "pl",
-      "pt-BR",
-      "ru",
-      "sl",
-      "sq",
-      "so",
-      "sv",
-      "val",
-      "zh-CN",
-      "zh-TW"]
+      ]
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = {
       "fr"    => "es",
